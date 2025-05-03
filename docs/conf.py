de6078edf1bc7be2081b2c862a86e9ff467102cb
Tiwalun <ziegeler.nilsjonas@fh-swf.de>
2025-05-03 22:16:00 +0200

    Bump version to 1.1 in configuration files

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(".."))  # Add project root
 
 project = "PyRth"
 author = "Nils J. Ziegeler"
-release = "1.0.2"
+release = "1.1"
 
 extensions = [
     "sphinx.ext.autodoc",
